6796b9a440eb7fbc27db3bee26003a267316798b
gaston <gastoncasini@gmail.com>
2020-03-17 11:21:38 -0300

    questions and vendor cards mockup

--- src/components/QuestionsCard/index.js
+++ src/components/QuestionsCard/index.js
@@ -1,6 +1,52 @@
 import React from "react";
 import Card from "../Card";
+import styles from "./questions.module.css";
+import "./styles.css";
 
-export default function QuestionsCard() {
-  return <Card />;
+function Question({ quest, ans }) {
+  function TextBlock({ user, content, type }) {
+    type =
+      type === "ans"
+        ? "text-block text-block--yellow text-block--right"
+        : "text-block";
+
+    return (
+      <div className={type}>
+        <div className={styles.user}>{user}</div>
+        <div className={styles.content}>{content}</div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <TextBlock type={"quest"} {...quest} />
+      {ans && <TextBlock type={"ans"} {...ans} />}
+    </>
+  );
+}
+
+export default function QuestionsCard({ questions }) {
+  let testQuestions = [
+    {
+      quest: {
+        user: "lolocololo",
+        content: "hola cual es el costo del envio ?"
+      },
+      ans: {
+        user: "shoes inc",
+        content: "el envio es gratis papa ! "
+      }
+    }
+  ];
+
+  questions = questions ? questions : testQuestions;
+
+  function renderQuestions(quest) {
+    return quest.map(props => {
+      return <Question {...props} />;
+    });
+  }
+
+  return <Card>{renderQuestions(questions)}</Card>;
 }
--- src/components/VendorCard/index.js
+++ src/components/VendorCard/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 import Card from "../Card";
+import styles from "./vendor.module.css";
 
-export default function VendorCard() {
-  return <Card />;
+export default function VendorCard({ name, image, branding }) {
+  const initialProps = {
+    name: "Shoes inc",
+    branding: "15 years delivering high quality shoes ",
+    image:
+      "https://cdn.dribbble.com/users/230290/screenshots/6617275/db_drb.jpg"
+  };
+
+  image = image ? image : initialProps.image;
+  name = name ? name : initialProps.name;
+  branding = branding ? branding : initialProps.branding;
+
+  return (
+    <Card>
+      <img src={image} alt="vendor image" className={styles.image} />
+      <h2>{name}</h2>
+      <p>{branding}</p>
+    </Card>
+  );
 }
