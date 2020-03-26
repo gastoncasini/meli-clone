7421e16ec3e28a2536891c0601dd3e38743270e5
Gaston Casini <gastoncasini@gmail.com>
2020-03-26 16:14:51 -0300

    productsDuck file

---
+++ src/redux/productsDuck.js
@@ -0,0 +1,13 @@
+// constants
+const initialData = {};
+
+// reducer
+
+export default function reducer(state = initialData, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+// actions
