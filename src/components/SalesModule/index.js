ea5479d1045b7a0497efa91dbcd3f6385c227f35
Gaston Casini <gastoncasini@gmail.com>
2020-03-31 16:03:06 -0300

    fully functional select quantity input

--- src/components/SalesModule/index.js
+++ src/components/SalesModule/index.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
+function Select({ options, label, classes, onChange }) {
+  return (
+    <>
+      <label htmlFor="" className={classes.label}>
+        {label}
+      </label>
+
+      <select name="" id="" className={classes.select} onChange={onChange}>
+        {options.map((value) => (
+          <option value={value}>{value}</option>
+        ))}
+      </select>
+    </>
+  );
+}
+
 export default function SalesModule() {
+  let [quantity, setQuantity] = useState(1);
+
+  function selectQuantity(e) {
+    setQuantity(e.target.value);
+  }
+
   return (
     <div className="sales">
       <div className="sales__payment-info">Hasta 12 cuotas sin interes</div>
 
       <div className="sales__quantity">
-        <label htmlFor="" className="sales__quantity__label">
-          Cantidad
-        </label>
-        <input
-          type="number"
-          min="1"
-          max="99"
-          value="1"
-          className="sales__quantity__input"
+        <Select
+          value={quantity}
+          options={[1, 2, 3, 4]}
+          label={"cantidad"}
+          classes={{
+            label: "sales__quantity__label",
+            select: "sales__quantity__select"
+          }}
+          onChange={selectQuantity}
         />
       </div>
       <button className="sales__btn sales__btn--with-bg-color">
