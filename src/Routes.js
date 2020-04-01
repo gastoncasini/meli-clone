e2a548da446cf788e44b419d85b9a7a7ce4c2f01
Gaston Casini <gastoncasini@gmail.com>
2020-04-01 09:08:48 -0300

    added shopping cart route and component

--- src/Routes.js
+++ src/Routes.js
@@ -4,11 +4,13 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Catalog from "./components/Catalog";
 import Product from "./pages/Product";
+import ShoppingCart from "./pages/ShoppingCart";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path={"/"} component={Home} />
+      <Route path={"/checkout"} component={ShoppingCart} />
       <Route path={"/login"} component={Login} />
       <Route path={"/products/:n"} component={Catalog} />
       <Route path={"/product/:id"} component={Product} />
---
+++ src/pages/ShoppingCart/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function MultipeStepForm() {
+  return <h1>hola nena</h1>;
+}
+
+export default function ShoppingCart() {
+  return <MultipeStepForm />;
+}
